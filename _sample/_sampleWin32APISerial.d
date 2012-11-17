7ec0fe256100ff2280325856a1e80bd3aa8278c7
k3_kaimu <enjouzensyou.boinc@gmail.com>
2012-11-17 23:39:36 +0900

    Win32APIでの通信を使えるようにしました

---
+++ _sample/_sampleWin32APISerial.d
@@ -0,0 +1,22 @@
+﻿/**
+ * diodeはdioをベースとしたライブラリです
+ */
+
+import diode.windows;
+import dio.core;
+import dio.port;
+import core.thread : Thread, dur;
+
+pragma(lib, "diode");
+pragma(lib, "dio");
+pragma(lib, "win32");
+
+void main(){
+    auto dev = comPortOpen("COM6");
+    
+    auto devRng = dev.buffered().ranged();
+    auto devPort = dev.buffered().textPort();
+    
+    foreach(d; devRng)
+        writeln(d);
+}
--- windows.d
+++ windows.d
@@ -6,15 +6,19 @@ module diode.windows;
 import std.conv                 : to;
 import std.c.windows.windows;
 import std.string               : toStringz;
+import std.exception            : enforce;
 //import std.c.windows.windows;
-import win32.all : DCB, CreateFile, GetCommState, CBR_9600, NOPARITY, ONESTOPBIT;
+import win32.all : DCB, CreateFile, GetCommState, CBR_9600, NOPARITY, ONESTOPBIT, COMMTIMEOUTS, SetCommState, GetCommTimeouts, SetCommTimeouts;
 
-@property serialPortOpen(string name){
+
+@property comPortOpen(string name){
+    
     static struct SerialPort
     {
     private:
         HANDLE  _handle;
         DCB*     _dcb;
+        COMMTIMEOUTS* _cto;
     
     public:
         this(string name)
@@ -25,7 +29,7 @@ import win32.all : DCB, CreateFile, GetCommState, CBR_9600, NOPARITY, ONESTOPBIT
                                     0,
                                     null,
                                     OPEN_EXISTING,
-                                    FILE_FLAG_OVERLAPPED,
+                                    FILE_ATTRIBUTE_NORMAL,
                                     null);
             assert(_handle != INVALID_HANDLE_VALUE, name ~ ", could not open.");
             
@@ -33,13 +37,33 @@ import win32.all : DCB, CreateFile, GetCommState, CBR_9600, NOPARITY, ONESTOPBIT
             
             with(*_dcb){
                 DCBlength = typeof(*_dcb).sizeof;
-                BaudRate = CBR_9600;
+                BaudRate = 9600;
                 fBinary = true;
                 fParity = false;
                 ByteSize = 8;
                 Parity = NOPARITY;
-                StopBits = ONESTOPBIT;
+                StopBits = ONESTOPBIT;/*
+                dcb.fOutxCtsFlow = 0;
+                dcb.fOutxDsrFlow = 0;
+                dcb.fDtrControl = DTR_CONTROL_DISABLE;
+                */
             }
+            
+            enforce(SetCommState(_handle, _dcb));
+            
+            _cto = new COMMTIMEOUTS;
+            GetCommTimeouts(_handle, _cto);
+            
+            with(*_cto)
+            {
+                ReadIntervalTimeout         = 10;		// タイムアウト：0.01秒	　
+                ReadTotalTimeoutMultiplier  = 0;
+                ReadTotalTimeoutConstant    = 10;
+                WriteTotalTimeoutMultiplier = 0;
+                WriteTotalTimeoutConstant   = 0;
+            }
+            
+            SetCommTimeouts(_handle, _cto);
         }
         
         
@@ -48,7 +72,48 @@ import win32.all : DCB, CreateFile, GetCommState, CBR_9600, NOPARITY, ONESTOPBIT
             CloseHandle(_handle);
         }
         
+        @property
+        HANDLE handle()
+        {
+            return _handle;
+        }
+            
+                ///Sourceのpullの実装
+        bool pull(ref ubyte[] buf){
+            DWORD cnt = void;
+            
+            enforce(ReadFile(_handle, cast(ubyte*)(buf.ptr), buf.length, &cnt, null));
+            
+            buf = buf[cnt .. $];
+            return cnt > 0;
+        }
         
+        
+        ///Sinkのpushの実装
+        bool push(ref const(ubyte)[] buf){
+            DWORD cnt = void;
+            
+            enforce(WriteFile(_handle, cast(ubyte*)(buf.ptr), buf.length, &cnt, null));
+            
+            buf = buf[cnt .. $];
+            return cnt > 0;
+        }
+        
+        
+        ///ボーレート
+        @property uint baudRate()
+        {
+            GetCommState(_handle, _dcb);
+            return _dcb.BaudRate;
+        }
+        
+        
+        ///ditto
+        @property void baudRate(uint rate)
+        {
+            _dcb.BaudRate = rate;
+            enforce(SetCommState(_handle, _dcb));
+        }
         
     }
     
